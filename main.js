fcfd4128d35b5bbab9fcc888012e39065f97da73
ArnoldFernando <115635149+ArnoldFernando@users.noreply.github.com>
2023-06-12 12:35:29 +0800

    ARNOLD's GROUP

---
+++ main.js
@@ -0,0 +1,15 @@
+function hello() {
+  alert("Hello!");
+}
+
+function contactInfo() {
+  alert("email: others@gmail.com");
+  alert("cellphone no. 09355529021")
+}
+
+
+// dark mode
+function darkmode_btn() {
+  let body = document.body;
+  body.classList.toggle("dark-mode");
+}
